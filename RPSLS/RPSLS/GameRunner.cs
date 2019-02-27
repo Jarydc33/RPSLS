506d52d8467250cf8577e9aeb910e87248f7b019
unknown <jarydc333@gmail.com>
2019-02-27 13:20:00 -0600

    At this point, I have the basic layout of how I want to implement the actual game function. I have been throwing around the idea of different ways to go about it, but I think I am going to have different methods for each type of throw

--- RPSLS/RPSLS/GameRunner.cs
+++ RPSLS/RPSLS/GameRunner.cs
@@ -11,15 +11,18 @@ namespace RPSLS
         PlayerBuilder Player2;
         ComputerBuilder CompPlayer;
         List<string> Gestures;
+        int TotalScore = 0;
+        int UserThrow;
+        int CompThrow;
 
         public GameRunner()
         {
             Gestures = new List<string>();
-            Gestures.Add("Rock");
-            Gestures.Add("Paper");
-            Gestures.Add("Scissors");
-            Gestures.Add("Lizard");
-            Gestures.Add("Spock");
+            Gestures.Add("Rock"); //case 1
+            Gestures.Add("Paper"); //case 2
+            Gestures.Add("Scissors"); //case 3
+            Gestures.Add("Lizard"); //case 4
+            Gestures.Add("Spock"); //case 5
         }
 
         public void UserChoicePlayers()
@@ -27,7 +30,19 @@ namespace RPSLS
             Console.WriteLine("Would you like to play against a (1) computer or a (2) person?");
             int Players = int.Parse(Console.ReadLine());
 
-            ChoosePlayers(Players);
+            switch (Players)
+            {
+                case 1:
+                    ChoosePlayers(Players);
+                    break;
+                case 2:
+                    ChoosePlayers(Players);
+                    break;
+                default:
+                    Console.WriteLine("I know you`re trying to break my code here (probably Nevin). Look elsewhere.");
+                    UserChoicePlayers();
+                    break;
+            }
         }
 
         public void ChoosePlayers(int numPlayers)
@@ -36,14 +51,68 @@ namespace RPSLS
             {
                 Player1 = new PlayerBuilder(0);
                 CompPlayer = new ComputerBuilder(0); // I am placing this 0 here in case I want to add a handicap
+                Console.WriteLine("Ok cool, you`ll be playing against the computer. We`ll call him..Dave. He is very good I must warn you.");
             }
             else
             {
                 Player1 = new PlayerBuilder(0);
                 Player2 = new PlayerBuilder(0);
             }
+
+            PlayGame();
         }
 
+        public void PlayGame()
+        {
+            do
+            {
+                Console.WriteLine("Would you like to throw (1) Rock, (2) Paper, (3) Scissors, (4) Lizard, or (5) Spock? " +
+                    "Press the corresponding number to choose.");
+                if (NumPlayers == 1)
+                {
+                    UserThrow = int.Parse(Console.ReadLine());
+                    CompThrow = CompPlayer.ThrowRandomizer();
+                }
+                else
+                {
+                    
+                }
+
+                if(UserThrow == CompThrow)
+                {
+                    Console.WriteLine("Looks like it was a tie! Time to start the round again!");
+                    PlayGame();
+                }
+
+                switch (UserThrow)
+                {
+                    case 1:
+                        
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+                }        
+            } while (TotalScore != 3);
+        }
+
+        public void RockResults(int UserChoice, int CompChoice)
+        {
+
+        }
 
     }
 }
